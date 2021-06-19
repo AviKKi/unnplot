ff12c0b34f4f69da1a19e535f3e5532cc7876ed5
Atul Yadav <atul7555@gmail.com>
2021-06-19 15:29:45 +0530

    Google Tag Manager added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,19 @@
-import React from "react"
+import React, { useEffect } from "react"
 import t from "prop-types"
 import "../styles/tailwind.css"
 import Theme from "../styles/Theme.js"
 import { ThemeProvider } from "@wigxel/react-components"
+import TagManager from "react-gtm-module"
 
+const tagManagerArgs = {
+  id: "G-HXGHE3RCG5",
+}
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    TagManager.initialize(tagManagerArgs)
+  }, [])
+
   return (
     <ThemeProvider theme={Theme}>
       <Component {...pageProps} />
--- src/pages/support.js
+++ src/pages/support.js
@@ -1,6 +1,5 @@
 import Head from "next/head"
 import React from "react"
-import Button from "../components/Button"
 import Header from "../components/Header"
 
 const Li = ({ children }) => (
